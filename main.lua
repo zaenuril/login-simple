74c6280e617de4503539122785bab49a6dac0bca
zaenuril hakim <zainurilhakim10188@gmail.com>
2025-11-19 07:20:35 +0700

    Refactor: cleaning the messy code

--- main.lua
+++ main.lua
@@ -22,58 +22,61 @@ local function main()
     while not logged_in and attempt < 3 do
 
     -- TO-DO tampilkan menu login user
-    if user_choice == 1 then
+        if user_choice == 1 then
 
+            
+        print("\n\n[=====] LOGIN [=====]\n")
+            
+            io.write("Username: ")
+            local input_user = io.read()
+            io.write("Password: ")
+            local input_pass = io.read()
+            io.write("Role: ")
+            local input_role = io.read()
+            
         
-    print("\n\n[=====] LOGIN [=====]\n")
-        
-        io.write("Username: ")
-        local input_user = io.read()
-        io.write("Password: ")
-        local input_pass = io.read()
-        io.write("Role: ")
-        local input_role = io.read()
-        
-    
-    -- menampilkan halaman login untuk user 
-    for _, userdata in pairs(data) do
-        -- print("username: " .. userdata.users .. " password: " .. userdata.pass .. " role: " ..userdata.role)
-        if userdata.users == input_user and userdata.pass == input_pass and userdata.role == input_role then
-            print("\n\n\n\nlogged on! welcome " .. userdata.users .. " your role is " .. userdata.role .. "\n")
-            print("[=====]     SELAMAT DATANG DI TEMPEST LOGIN SYSTEM    [=====]")
-            logged_in = true
-        end
+            -- menampilkan halaman login untuk user 
+            for _, userdata in pairs(data) do
+                if userdata.users == input_user and userdata.pass == input_pass and userdata.role == input_role then
+                    print("\n\n\n\n[=====] logged on! welcome " .. userdata.users .. " your role is " .. userdata.role .. " [=====]\n")
+                    print("[=====]     SELAMAT DATANG DI TEMPEST LOGIN SYSTEM        [=====]")
+                    logged_in = true
+                end
 
-        -- cek kalau user salah memasukan username
-        if userdata.users ~= input_user and userdata.pass == input_pass and userdata.role == input_role then
-            print("\n\n\n\nplease input correct username")
-        end
+                -- cek kalau user salah memasukan username
+                if userdata.users ~= input_user and userdata.pass == input_pass and userdata.role == input_role then
+                    print("\n\n\n\nplease input correct username")
+                end
 
-        if userdata.users == input_user and userdata.pass ~= input_pass and userdata.role == input_role then
-        -- -- cek kalau user salah memasukan password
-        --  local converted_pass = tonumber(input_pass) -- converting 'input_pass' to a number
-        -- if converted_pass ~= nil then
-        attempt = attempt + 1
-
-            print("please input correct password!")
-        end
+                if userdata.users == input_user and userdata.pass ~= input_pass and userdata.role == input_role then
+                    attempt = attempt + 1
+                    -- print("please input correct password! " )
+                    print("\nyou've wrong " .. attempt.. "x, are u sure it's your password?")
+                end
                 if attempt == 3 then
-            print("you've input max attempt, program will be terminated!")
+                    print("you've input max attempt, program will be terminated!")
+                end
+                
+                if userdata.users == input_user and userdata.pass == input_pass and userdata.role ~= input_role then
+                    print("please input correct role!")
+                end
+                
+            end
+        
+        elseif user_choice == 2 then
+            print("nanti ya, males bikin")
+            logged_in = true
+
+        else
+            print("apakah yakin ingin keluar? [Y/y]")
+            local keluar = io.read()
+        
+            if keluar == 'Y' or keluar == 'y' then
+                print("berhasil keluar")
+                logged_in = true
+            end
         end
     end
-
-        -- end
-    -- end
-    
-elseif user_choice == 2 then
-    print("menampilkan menu register")
-else
-            print("berhasil keluar")
-            -- TO-DO jika user gagal login
-        end
-
-
-end
 end
 
 main()
